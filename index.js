450a3aa6b4e17cef177a5104cc715da542acdd2c
Lyncy Hodge <lyncyyhodge@gmail.com>
2023-10-11 19:46:39 -0500

    8.2 Aug 2023 Cohort

--- index.js
+++ index.js
@@ -1,69 +1,103 @@
 import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
-
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
 
 const router = new Navigo("/");
 
 function render(state = store.Home) {
+  // for (let key in state) {
+  //   console.log("Key: " + key + "  Value: " + state[key]);
+  // }
   document.querySelector("#root").innerHTML = `
-  ${Header(state)}
-  ${Nav(store.Links)}
-  ${Main(state)}
-  ${Footer()}
-`;
-
-  router.updatePageLinks();
-
-  afterRender(state);
+    ${Header(state)}
+    ${Nav(store.Links)}
+    ${Main(state)}
+    ${Footer()}
+  `;
+  afterRender();
   router.updatePageLinks();
 }
 
-function afterRender(state) {
-  // add event listeners to Nav items for navigation
-  document.querySelectorAll("nav a").forEach(navLink =>
-    navLink.addEventListener("click", event => {
-      event.preventDefault();
-      render(store[event.target.title]);
-    })
-  );
-
+function afterRender() {
   // add menu toggle to bars icon in nav bar
-  document
-    .querySelector(".fa-bars")
-    .addEventListener("click", () =>
-      document.querySelector("nav > ul").classList.toggle("hidden--mobile")
-    );
-
-  if (state.view === "Order") {
-    document.querySelector("form").addEventListener("submit", event => {
-      event.preventDefault();
-      const inputList = event.target.elements;
-
-      const toppings = [];
-      for (let input of inputList.toppings) {
-        if (input.checked) {
-          toppings.push(input.value);
-        }
-      }
-      const requestData = {
-        crust: inputList.crust.value,
-        cheese: inputList.cheese.value,
-        sauce: inputList.sauce.value,
-        toppings: toppings,
-        customer: "~Update with YOUR name~"
-      };
-
-      store.Pizza.pizzas.push(requestData);
-      router.navigate("/Pizza");
-    });
-  }
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
 }
+
+router.hooks({
+  before: (done, params) => {
+    // We need to know what view we are on to know what data to fetch
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+    // Add a switch case statement to handle multiple routes
+    switch (view) {
+      // Add a case for each view that needs data from an API
+      // New Case for the Home View
+      case "Home":
+        axios
+          // Get request to retrieve the current weather data using the API key and providing a city name
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
+          )
+          .then(response => {
+            // Convert Kelvin to Fahrenheit since OpenWeatherMap does provide otherwise
+            const kelvinToFahrenheit = kelvinTemp =>
+              Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+            // console.log(response.data);
+            // Create an object to be stored in the Home state from the response
+            store.Home.weather = {
+              city: response.data.name,
+              temp: kelvinToFahrenheit(response.data.main.temp),
+              feelsLike: kelvinToFahrenheit(response.data.main.feels_like),
+              description: response.data.weather[0].main
+            };
+
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+          });
+        break;
+
+      case "Pizza":
+        // New Axios get request utilizing already made environment variable
+        axios
+          .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
+          .then(response => {
+            // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
+            store.Pizza.pizzas = response.data;
+            console.log(store.Pizza);
+            done();
+          })
+          .catch(error => {
+            console.log("It puked", error);
+            done();
+          });
+        break;
+      default:
+        done();
+    }
+  },
+  already: params => {
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home";
+
+    render(store[view]);
+  }
+});
 
 router
   .on({
-    "/": () => render(),
+    "/": () => render(store.Home),
     ":view": params => {
       let view = capitalize(params.data.view);
       if (view in store) {
